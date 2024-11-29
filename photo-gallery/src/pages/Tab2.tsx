ee913d4a7cc3b24985e099013b9fa55533aa854b
Kelly Durkin <kdurkin@bearcubs.santarosa.edu>
2024-11-29 09:52:54 -0800

    working on tab2 - built usePhotoGallery.ts hook

--- photo-gallery/src/pages/Tab2.tsx
+++ photo-gallery/src/pages/Tab2.tsx
@@ -1,6 +1,24 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+  IonActionSheet,
+} from '@ionic/react';
+
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+import { camera, trash, close } from 'ionicons/icons';
+
 
 const Tab2: React.FC = () => {
   return (
@@ -17,7 +35,15 @@ const Tab2: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 2 page" />
+
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+    <IonFabButton onClick={() => takePhoto()}>
+      <IonIcon icon={camera}></IonIcon>
+    </IonFabButton>
+  </IonFab>
       </IonContent>
+      
+      
     </IonPage>
   );
 };
