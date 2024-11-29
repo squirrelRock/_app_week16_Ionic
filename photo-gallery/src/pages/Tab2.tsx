3ab90fa6c8642d756bf8d17a2426680efbf29d53
Kelly Durkin <kdurkin@bearcubs.santarosa.edu>
2024-11-29 10:56:50 -0800

    fixed photo gallery grid

--- photo-gallery/src/pages/Tab2.tsx
+++ photo-gallery/src/pages/Tab2.tsx
@@ -41,6 +41,16 @@ const Tab2: React.FC = () => {
         </IonHeader>
         <ExploreContainer name="Tab 2 page" />
 
+        <IonGrid>
+    <IonRow>
+      {photos.map((photo, index) => (
+        <IonCol size="6" key={photo.filepath}>
+          <IonImg src={photo.webviewPath} />
+        </IonCol>
+      ))}
+    </IonRow>
+  </IonGrid>
+
         <IonFab vertical="bottom" horizontal="center" slot="fixed">
     <IonFabButton onClick={() => takePhoto()}>
       <IonIcon icon={camera}></IonIcon>
